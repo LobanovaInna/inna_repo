cab727fe0e545248ccb569d92c6b758522d50338
LobanovaInna <49236810+LobanovaInna@users.noreply.github.com>
2025-04-24 12:11:08 +0000

    functions

--- src/basic_python.py
+++ src/basic_python.py
@@ -1,3 +1,10 @@
 age = 35
-my_age = age + 1
-print('My age in 1 year: ', my_age)
+Alex_age = 41
+def Age_calculator (age):
+    my_age = age + 1
+    print('My age in 1 year: ', my_age)
+
+Age_calculator (35)
+Age_calculator (test)
+Age_calculator (age)
+Age_calculator (Alex_age)
